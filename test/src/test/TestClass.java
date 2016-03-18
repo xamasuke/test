7af50aba8be3d2ef2d3bd7a48bd530106cdb7ffb
Yusuke <xamasuke@gmail.com>
2016-03-18 14:09:33 +0900

    Test commit

---
+++ test/src/test/TestClass.java
@@ -0,0 +1,17 @@
+package test;
+
+/**
+ * Test class to bind Eclipse with Github
+ * 
+ * @author Yusuke
+ *
+ */
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("HelloWorld!");
+
+	}
+
+}
